35a0067bc26347736d0072d7ca85ec25dd8e88d0
Tadej Ferk <tadex121@gmail.com>
2021-02-25 11:27:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
 
 function App() {
+  const [operation, setOperation] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form name='calc'>
+      <table>
+        <input
+          type='text'
+          name='input'
+          size='16'
+          className='input'
+          placeholder='0'
+          value={operation}
+        ></input>
+        <tr>
+          <td>
+            <input
+              type='button'
+              value='1'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='2'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='3'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='+'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <br></br>
+            <input
+              type='button'
+              value='4'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='5'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='6'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='-'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <br></br>
+            <input
+              type='button'
+              value='7'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='8'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='9'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              value='*'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <br></br>
+            <input
+              type='button'
+              id='clear'
+              name='clear'
+              value=' C '
+              onClick={() =>
+                setOperation(operation.substr(0, operation.length - 1))
+              }
+            />
+            <input
+              type='button'
+              name='zero'
+              value='0'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <input
+              type='button'
+              name='doit'
+              value=' = '
+              onClick={(e) => {
+                try {
+                  setOperation(
+                    String(eval(operation)).length > 3 &&
+                      String(eval(operation)).includes('.')
+                      ? String(eval(operation).toFixed(4))
+                      : String(eval(operation))
+                  )
+                } catch (error) {
+                  console.log(error)
+                }
+              }}
+            />
+            <input
+              type='button'
+              name='div'
+              value='/'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+            <br></br>
+            <input
+              type='button'
+              value='clear'
+              className='clear'
+              onClick={() => setOperation('')}
+            />
+            <input
+              type='button'
+              value='.'
+              onClick={(e) => {
+                setOperation(operation + e.target.value)
+              }}
+            />
+          </td>
+        </tr>
+      </table>
+    </form>
+  )
 }
 
-export default App;
+export default App
